25abaf0bd247444fcfa837109f12088114898d98
Zhihan Zhou <zhihan1996@users.noreply.huggingface.co>
2024-01-12 07:40:23 +0000

    Update bert_layers.py

--- bert_layers.py
+++ bert_layers.py
@@ -413,7 +413,7 @@ class BertEncoder(nn.Module):
 
         extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
         extended_attention_mask = extended_attention_mask.to(
-            dtype=next(self.parameters()).dtype)  # fp16 compatibility
+            dtype=torch.float32)  # fp16 compatibility
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
         attention_mask_bool = attention_mask.bool()
